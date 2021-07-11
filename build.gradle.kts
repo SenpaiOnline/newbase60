94ae47602af1aa42cea8ad51f61361ea9a6f5cf0
Maxim Pavlov <maxpav137@gmail.com>
2021-07-11 20:25:36 +0500

    build(NB60-1): bump Kotlin version; bump Gradle version; add me.qoomon.git-versioning plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.4.31"
+    kotlin("jvm") version "1.5.20"
+    id("me.qoomon.git-versioning") version "4.2.1"
 }
-
-group = "online.senpai"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -11,4 +9,44 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    testImplementation("io.kotest:kotest-runner-junit5:4.6.0")
 }
+
+tasks {
+    compileKotlin {
+        kotlinOptions.languageVersion = "1.2"
+        kotlinOptions.jvmTarget = "6"
+    }
+    compileTestKotlin {
+        kotlinOptions.languageVersion = "1.2"
+        kotlinOptions.jvmTarget = "6"
+    }
+    test {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
+    }
+    processResources {
+        duplicatesStrategy = DuplicatesStrategy.FAIL
+    }
+}
+
+gitVersioning.apply(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig> {
+    branch(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig.VersionDescription> {
+        pattern = "main"
+        versionFormat = "\${version}"
+    })
+    branch(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig.VersionDescription> {
+        pattern = "feature/(?<feature>.+)"
+        versionFormat = "\${feature}-SNAPSHOT"
+    })
+    branch(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig.VersionDescription> {
+        pattern = "pull/(.+)"
+        versionFormat = "\${branch}-SNAPSHOT"
+    })
+    tag(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig.VersionDescription> {
+        pattern = "v(?<tagVersion>[0-9].*)"
+        versionFormat = "\${tagVersion}"
+    })
+})
