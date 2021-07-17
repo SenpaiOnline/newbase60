0838e5e33f568d109fee158ba6fcb96d935425af
Maxim Pavlov <maxpav137@gmail.com>
2021-07-17 15:37:32 +0500

    build(NB60-4): add the necessary things to be able to publish on Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.5.20"
+    `maven-publish`
+    signing
+    kotlin("jvm") version "1.5.21"
     id("me.qoomon.git-versioning") version "4.2.1"
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    id("org.jetbrains.dokka") version "1.5.0"
 }
 
 repositories {
@@ -33,6 +37,21 @@ tasks {
     processResources {
         duplicatesStrategy = DuplicatesStrategy.FAIL
     }
+
+    val sourcesJar: TaskProvider<Jar> by registering(Jar::class) {
+        archiveClassifier.set("sources")
+        from(sourceSets["main"].allSource)
+    }
+    val javadocJar: TaskProvider<Jar> by registering(Jar::class) {
+        dependsOn(dokkaHtml)
+        archiveClassifier.set("javadoc")
+        from(dokkaHtml)
+    }
+    artifacts {
+        archives(sourcesJar)
+        archives(javadocJar)
+        archives(jar)
+    }
 }
 
 gitVersioning.apply(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPluginConfig> {
@@ -53,3 +72,60 @@ gitVersioning.apply(closureOf<me.qoomon.gradle.gitversioning.GitVersioningPlugin
         versionFormat = "\${tagVersion}"
     })
 })
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            val ossrhUsername: String? by project
+            val ossrhPassword: String? by project
+            username.set(ossrhUsername)
+            password.set(ossrhPassword)
+        }
+    }
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+
+            pom {
+                packaging = "jar"
+                name.set("newbase60")
+                description.set("Yet another implementation of NewBase60.")
+                url.set("https://github.com/SenpaiOnline/newbase60")
+                scm {
+                    connection.set("scm:git:ssh://github.com/SenpaiOnline/newbase60.git")
+                    developerConnection.set("scm:git:ssh://github.com/SenpaiOnline/newbase60.git")
+                    url.set("https://github.com/SenpaiOnline/newbase60")
+                }
+                issueManagement {
+                    system.set("github")
+                    url.set("https://github.com/SenpaiOnline/newbase60/issues")
+                }
+                licenses {
+                    license {
+                        name.set("Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("aiscy")
+                        name.set("Maxim Pavlov")
+                        email.set("maxpav137@gmail.com")
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["mavenJava"])
+}
